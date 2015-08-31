83fff2ac62b87ff72d7491de5bec90fad3836629
Hans Petter Langtangen <hpl@simula.no>
2015-08-31 10:54:12 -0700

    First compilation of the document.

--- doc/src/make.sh
+++ doc/src/make.sh
@@ -1,16 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 # Usage: make.sh latex|sphinx
 
 name=ftut
 version=1.0
 version=1.1
 
-# Format
-if [ $# -eq 0 ]; then
-  target=latex
-else
-  target=$1
-fi
+set -x
+
+function system {
+  "$@"
+  if [ $? -ne 0 ]; then
+    echo "make.sh: unsuccessful command $@"
+    echo "abort!"
+    exit 1
+  fi
+}
 
 # We have Norwegian characters in the acknowledgement and this
 # requires latin1 for latex and utf-8 for rst/sphinx
@@ -24,9 +28,10 @@ fi
 #  fi
 #done
 #
-if [ $target = "latex" ]; then
+
 # Make latex
-doconce format latex $name --device=paper --latex_title_layout=std
+preprocess -DFORMAT newcommands_keep.p.tex > newcommands_keep.tex
+system doconce format pdflatex $name --device=paper --latex_title_layout=titlepage "--latex_code_style=default:vrb-blue1@sys:vrb[frame=lines,label=\\fbox{{\tiny Terminal}},framesep=2.5mm,framerule=0.7pt,fontsize=\fontsize{9pt}{9pt}]"
 
 #enc=`doconce guess_encoding $name.p.tex`
 #if [ $enc = "utf-8" ]; then
@@ -34,65 +39,23 @@ doconce format latex $name --device=paper --latex_title_layout=std
 #   doconce change_encoding utf-8 iso-8859-1 $name.p.tex
 #fi
 
-doconce replace 'ptex2tex}' 'ptex2tex,subfigure}' $name.p.tex
-doconce subst 'This document presents a' '\\tableofcontents\n\n\\clearpage\n\nThis document presents a' $name.p.tex
-ptex2tex $name
-latex $name
-bibtex $name
-makeindex $name
-latex $name
-makeindex $name
-latex $name
-# dvipdf gives error messages if $name.pdf is already open in evince...not serious
-dvipdf $name
-dest=fenics_tutorial${version}-4print
-cp $name.pdf ${dest}.pdf
+#doconce replace 'ptex2tex}' 'ptex2tex,subfigure}' $name.p.tex
+#doconce subst 'This document presents a' '\\tableofcontents\n\n\\clearpage\n\nThis document presents a' $name.p.tex
+system pdflatex $name
+system bibtex $name
+system makeindex $name
+pdflatex $name
+pdflatex $name
+cp $name.pdf ${name}-4print.pdf
 
-grep 'multiply defined' $name.log > ERRORS
-
-echo
-echo "Result: $dest.pdf"
-fi
-
-if [ $target = "sphinx" ]; then
+exit
 # Make sphinx
-# (Just remove sphinx-rootdir to have it recreated)
-
+preprocess -DHTML newcommands_keep.p.tex > newcommands_keep.tex
 dir=sphinx-rootdir
-rm -rf $dir
-doconce sphinx_dir dirname=$dir author='H. P. Langtangen' version=$version theme=fenics $name
-python automake_sphinx.py
+system doconce format sphinx $name
+system doconce split_rst $name.rst
+system doconce sphinx_dir dirname=$dir copyright='H. P. Langtangen' version=$version theme=fenics $name
+system python automake_sphinx.py
 
-dest=fenics_tutorial${version}-1
-rm -rf $dest
-cp -r $dir/_build/html .
-mv html $dest
-echo "Result: $dest/index.html"
-fi
-
-if [ $target = "sphinxm" ]; then
-# Make sphinx with multiple .rst files
-
-dir=sphinx-rootdir
-rm -rf $dir
-doconce format sphinx $name
-doconce split_rst $name.rst
-
-doconce sphinx_dir dirname=$dir author='H. P. Langtangen' version=$version theme=fenics $name
-python automake_sphinx.py
-
-dest=fenics_tutorial${version}
-rm -rf $dest
-cp -r $dir/_build/html .
-mv html $dest
-echo "Result: $dest/index.html"
-fi
-
-rm -rf ._*
-
-# Copy
-# rm -rf ~/vc/INF5620/doc/pub/fenics_tutorial1.1-1
-# cp -r fenics_tutorial1.1-1 ~/vc/INF5620/doc/pub
-# rm -rf ~/vc/INF5620/doc/pub/fenics_tutorial1.1
-# cp -r fenics_tutorial1.1 ~/vc/INF5620/doc/pub
-# cp $name.pdf ~/vc/INF5620/doc/pub/fenics_tutorial1.1-4print.pdf
+dest=../pub
+cp -r *.pdf sphinx-rootdir/_build/html $dest
