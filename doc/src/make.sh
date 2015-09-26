3dc187cddf92f3346fb1acb6e0ff458398e08df8
Hans Petter Langtangen <hpl@simula.no>
2015-09-26 06:53:06 -0700

    updates

--- doc/src/make.sh
+++ doc/src/make.sh
@@ -20,8 +20,8 @@ function system {
 # requires latin1 for latex and utf-8 for rst/sphinx
 
 # Make latex
-preprocess -DFORMAT newcommands_keep.p.tex > newcommands_keep.tex
-system doconce format pdflatex $name --device=paper --latex_title_layout=titlepage "--latex_code_style=default:vrb-blue1@sys:vrb[frame=lines,label=\\fbox{{\tiny Terminal}},framesep=2.5mm,framerule=0.7pt,fontsize=\fontsize{9pt}{9pt}]" --encoding=utf-8
+system preprocess -DFORMAT=pdflatex newcommands_replace.p.tex > newcommands_replace.tex
+system doconce format pdflatex $name --device=paper --latex_title_layout=titlepage "--latex_code_style=default:vrb-blue1@sys:vrb[frame=lines,label=\\fbox{{\tiny Terminal}},framesep=2.5mm,framerule=0.7pt,fontsize=\fontsize{9pt}{9pt}]" --encoding=utf-8 --latex_copyright=titlepages
 
 #doconce replace 'ptex2tex}' 'ptex2tex,subfigure}' $name.p.tex
 #doconce subst 'This document presents a' '\\tableofcontents\n\n\\clearpage\n\nThis document presents a' $name.p.tex
@@ -33,7 +33,7 @@ pdflatex $name
 cp $name.pdf fenics-tutorial-4print.pdf
 
 # Make sphinx
-preprocess -DHTML newcommands_keep.p.tex > newcommands_keep.tex
+preprocess -DFORMAT=html newcommands_replace.p.tex > newcommands_replace.tex
 dir=sphinx-rootdir
 system doconce format sphinx $name --encoding=utf-8
 system doconce split_rst $name.rst
