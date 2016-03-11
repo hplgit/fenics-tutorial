269650c8f740388cc2b47c65ee75100018500006
Hans Petter Langtangen <hpl@simula.no>
2016-03-11 00:06:42 -0800

    updates

--- doc/src/make.sh
+++ doc/src/make.sh
@@ -21,7 +21,7 @@ function system {
 
 # Make latex
 system preprocess -DFORMAT=pdflatex newcommands.p.tex > newcommands.tex
-system doconce format pdflatex $name --device=paper --latex_title_layout=titlepage "--latex_code_style=default:vrb-blue1@sys:vrb[frame=lines,label=\\fbox{{\tiny Terminal}},framesep=2.5mm,framerule=0.7pt,fontsize=\fontsize{9pt}{9pt}]" --encoding=utf-8 --latex_copyright=titlepages
+system doconce format pdflatex $name --device=paper --latex_title_layout=titlepage "--latex_code_style=default:vrb-blue1@sys:vrb[frame=lines,label=\\fbox{{\tiny Terminal}},framesep=2.5mm,framerule=0.7pt,fontsize=\fontsize{9pt}{9pt}]" --encoding=utf-8 --latex_copyright=titlepages --skip_inline_comments
 
 #doconce replace 'ptex2tex}' 'ptex2tex,subfigure}' $name.p.tex
 #doconce subst 'This document presents a' '\\tableofcontents\n\n\\clearpage\n\nThis document presents a' $name.p.tex
@@ -35,13 +35,13 @@ cp $name.pdf fenics-tutorial-4print.pdf
 # Make sphinx
 preprocess -DFORMAT=html newcommands.p.tex > newcommands.tex
 dir=sphinx-rootdir
-system doconce format sphinx $name --encoding=utf-8
+system doconce format sphinx $name --encoding=utf-8 --skip_inline_comments
 system doconce split_rst $name.rst
 system doconce sphinx_dir dirname=$dir version=$version theme=fenics $name
 system python automake_sphinx.py
 
 # Make Bootstrap HTML
-system doconce format html $name --encoding=utf-8 --html_style=bootswatch_journal
+system doconce format html $name --encoding=utf-8 --html_style=bootswatch_journal --skip_inline_comments
 system doconce split_html $name.html --pagination
 
 dest=../pub
