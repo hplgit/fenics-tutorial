f0029041c055a186a71e8bef2bc8725c53dc7638
Anders Logg <logg@chalmers.se>
2016-03-22 16:00:49 +0100

    Half-revert simplified build script (still skipping Sphinx) after hack to get doconce to run (hack in get_legal_pygments_lexers).

--- doc/src/make_simple.sh
+++ doc/src/make_simple.sh
@@ -5,15 +5,50 @@ name=ftut
 version=1.0
 version=1.1
 
-# Doconce
-preprocess -DFORMAT=pdflatex newcommands.p.tex > newcommands.tex
-doconce format pdflatex $name --device=paper --latex_title_layout=titlepage "--latex_code_style=default:lst[style=yellow2_fb]@sys:vrb[frame=lines,label=\\fbox{{\tiny Terminal}},framesep=2.5mm,framerule=0.7pt,fontsize=\fontsize{9pt}{9pt}]" --encoding=utf-8 --latex_copyright=titlepages --latex_section_headings=blue
+set -x
+
+function system {
+  "$@"
+  if [ $? -ne 0 ]; then
+    echo "make.sh: unsuccessful command $@"
+    echo "abort!"
+    exit 1
+  fi
+}
+
+rm -rf tmp_*.do.txt  # don't spellcheck old versions
+#system doconce spellcheck -d .dict4spell.txt *.do.txt
+doconce spellcheck -d .dict4spell.txt *.do.txt
+
+# We have Norwegian characters in the acknowledgement and this requires utf-8
+# Generate latex output
+system preprocess -DFORMAT=pdflatex newcommands.p.tex > newcommands.tex
+system doconce format pdflatex $name --device=paper --latex_title_layout=titlepage "--latex_code_style=default:lst[style=yellow2_fb]@sys:vrb[frame=lines,label=\\fbox{{\tiny Terminal}},framesep=2.5mm,framerule=0.7pt,fontsize=\fontsize{9pt}{9pt}]" --encoding=utf-8 --latex_copyright=titlepages --latex_section_headings=blue
+# Fix: make full box around code blocks a la the FEniCS book
 doconce replace 'frame=tb,' 'frame=tblr,' $name.tex
 
-# LaTeX
-pdflatex $name
-bibtex $name
-makeindex $name
+# Compile latex
+system pdflatex $name
+system bibtex $name
+system makeindex $name
 pdflatex $name
 pdflatex $name
 cp $name.pdf fenics-tutorial-4print.pdf
+
+# Make sphinx
+#preprocess -DFORMAT=html newcommands.p.tex > newcommands.tex
+#dir=sphinx-rootdir
+#system doconce format sphinx $name --encoding=utf-8
+#system doconce split_rst $name.rst
+#system doconce sphinx_dir dirname=$dir version=$version theme=fenics $name
+#system python automake_sphinx.py
+
+# Make Bootstrap HTML
+#system doconce format html $name --encoding=utf-8 --html_style=bootswatch_journal
+#system doconce split_html $name.html --pagination
+
+#dest=../pub
+#rm -rf $dest/sphinx
+#cp -r fenics-tutorial-4print.pdf sphinx-rootdir/_build/html $dest
+#mv -f $dest/html $dest/sphinx
+#cp -r $name.html ._*.html fig $dest
