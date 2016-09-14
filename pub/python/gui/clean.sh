57fcf58c581f5e69a74e95321adc3a4e6a83db79
Anders Logg <logg@chalmers.se>
2016-09-14 23:14:47 +0200

    Remove old stuff in pub to be replaced by new generated stuff.

--- pub/python/gui/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Clean up files that can be regenerated
-rm -rf uploads/ templates/ static/ controller.py model.py *.pyc *~ clean.sh
--- pub/python/gui/generate.py
+++
@@ -1,5 +0,0 @@
-# Concepts: p2D_func, parameters, XML file, plotting in batch
-from parampool.generator.flask import generate
-from compute import compute, define_pool
-
-generate(compute, pool_function=define_pool, MathJax=True)
